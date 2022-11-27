c5c1039bab1862058f16f08e49e3996fbc03459b
stefang <marelestef@gmail.com>
2022-11-27 11:46:58 +0100

    deploy backend

--- app/test1/page.jsx
+++ app/test1/page.jsx
@@ -4,6 +4,7 @@ const { NEXT_PUBLIC_VERCEL_URL } = process.env
 const url = NEXT_PUBLIC_VERCEL_URL ? `https://${NEXT_PUBLIC_VERCEL_URL}/api/test1` : 'http://localhost:3000/api/test1'
 console.log('URL', NEXT_PUBLIC_VERCEL_URL, url)
 async function getPosts () {
+  return 'strings'
   const response = await fetch(url)
   const data = await response.json()
   return data
