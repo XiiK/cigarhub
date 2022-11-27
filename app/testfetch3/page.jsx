d6e4664a5408d36d8d5fcf4f8996036a73a5e838
stefang <marelestef@gmail.com>
2022-11-27 11:24:20 +0100

    test4

--- app/testfetch3/page.jsx
+++ app/testfetch3/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 const { NODE_ENV } = process.env
-const url = NODE_ENV === 'production' ? 'https://cigarhub.vercel.app/api/test1' : 'http://localhost:3000/api/test1'
+const url = NODE_ENV === 'production' ? '/api/test1' : 'http://localhost:3000/api/test1'
 // console.log('NODE_ENV', NODE_ENV)
 async function getPosts () {
   const response = await fetch(url)
-  console.log('response', response)
   const data = await response.json()
   return data
 }
