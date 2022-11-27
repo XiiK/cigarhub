51f1157519f71880568b5df0efbb8f2a730bca0c
stefang <marelestef@gmail.com>
2022-11-27 11:49:05 +0100

    deploy backend2

--- app/test1/page.jsx
+++ app/test1/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-const { NEXT_PUBLIC_VERCEL_URL } = process.env
+const { NODE_ENV } = process.env
 
-const url = NEXT_PUBLIC_VERCEL_URL ? `https://${NEXT_PUBLIC_VERCEL_URL}/api/test1` : 'http://localhost:3000/api/test1'
-console.log('URL', NEXT_PUBLIC_VERCEL_URL, url)
+const url = NODE_ENV === 'production' ? 'https://cigarhub.vercel.app/api/test1' : 'http://localhost:3000/api/test1'
+
 async function getPosts () {
-  return 'strings'
+  // return 'strings'
   const response = await fetch(url)
   const data = await response.json()
   return data
