e3c7c68150101214cc8165701c271411720859c6
stefang <marelestef@gmail.com>
2022-11-29 17:54:03 +0100

    url signin

--- app/auth/signin/SignInComponent.jsx
+++ app/auth/signin/SignInComponent.jsx
@@ -2,14 +2,12 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 const SignInComponent = ({ providers }) => {
+  const callbackUrl = (process.env.VERCEL_URL || 'http://localhost:3000') + '/home'
   return (
     <div>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, {
-            callbackUrl: (process.env.VERCEL_URL + '/home') || 'http://localhost:3000/home'
-          })}
-          >Sign in with {providers.name}
+          <button onClick={() => signIn(provider.id, { callbackUrl })}>Sign in with {providers.name}
           </button>
         </div>
       ))}
