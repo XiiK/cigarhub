9becea5b75a5ed97ba2a9ec823b0e80c240ee7ca
stefang <marelestef@gmail.com>
2022-11-29 17:03:29 +0100

    test deploy

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,6 +1,6 @@
 import Navbar from './Navbar'
 import ProvidersWrapper from './ProvidersWrapper'
-import Test from './Test'
+
 export default function HomeLayout ({ children }) {
   return (
     <ProvidersWrapper>
