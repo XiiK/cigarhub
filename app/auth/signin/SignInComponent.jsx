e988c923ff66ce1fb543a724c5dbafa8fb3a23d3
stefang <marelestef@gmail.com>
2022-11-29 19:53:15 +0100

    saadssd

--- app/auth/signin/SignInComponent.jsx
+++ app/auth/signin/SignInComponent.jsx
@@ -4,11 +4,13 @@ import { signIn } from 'next-auth/react'
 const callbackUrl = process.env.NEXT_PUBLIC_SITE_URL + '/home'
 
 const SignInComponent = ({ providers }) => {
+  console.log('providers', providers)
+  console.log('callbackUrl', callbackUrl)
   return (
     <div>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, { callbackUrl })}>Sign in with {providers.name}
+          <button onClick={() => signIn(provider.id, { callbackUrl: '/home' })}>Sign in with {providers.name}
           </button>
         </div>
       ))}
