74368f717ffdfd853b12c5e0313db863d89d7e42
stefang <marelestef@gmail.com>
2022-11-30 11:27:42 +0100

    Added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: 'Atlanta, sans-serif'
+      }
+    }
   },
   plugins: []
 }
