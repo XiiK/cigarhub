b5df274a42293ddd6404a3ac9a1a69f20b20886c
Federico Schianchi <federicoschianchi2004@gmail.com>
2022-12-01 11:30:36 +0100

    navbar hero sectiong changes

--- app/home/friends/page.jsx
+++ app/home/friends/page.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const PageFriends = () => {
+  const friendList = [
+    {
+      id: '0',
+      name: 'GiuppiTek',
+      age: 20
+    },
+    {
+      id: '1',
+      name: 'Goku',
+      age: 20
+    },
+    {
+      id: '2',
+      name: 'Vegeta',
+      age: 20
+    }
+  ]
+
   return (
-    <div>PageFriends</div>
+    <>
+      <div>PageFriends</div>
+      {friendList.map((friend) => (
+        <div key={friend.id}>
+          <div>{friend.name}</div>
+          <div>{friend.age}</div>
+        </div>
+      ))}
+    </>
   )
 }
 
