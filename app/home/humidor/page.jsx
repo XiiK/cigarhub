50bb2c79fb1330b68ff76b0f19438343804579c5
Federico Schianchi <federicoschianchi2004@gmail.com>
2023-01-15 17:33:20 +0100

    Humidor section responsive endedd

--- app/home/humidor/page.jsx
+++ app/home/humidor/page.jsx
@@ -12,14 +12,19 @@ const PageHumidor = async () => {
   console.log('process.env.NEXT_PUBLIC_SITE_URL', process.env.NEXT_PUBLIC_SITE_URL)
   return (
     <>
-      <div id='humidor-page' className='grid grid-cols-4 gap-10'>
+      <div id='humidor-page' className='grid grid-row-4 gap-10'>
         {cigars.brands.map((brand) => (
-          <div className='post' key={brand?.brand_id}>
-            <h1>{brand?.name}</h1>
-            <img src={brand?.img_logo} alt={brand?.brand_id} width={100} height={200} />
+          <div className='bg-white rounded-lg shadow-2xl md:flex' id='card' key={brand?.brand_id}>
+            <img className='rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none' src={brand?.img_logo} alt={brand?.brand_id} width={100} height={200} />
+            <div className='p-6'>
+              <h2 className='mb-2 font-bold text:xl md:text-2xl text-black-400'>{brand?.name}</h2>
+              <h3 className='mb-2 font-bold text:xl md:text-l text-gray-500'>{brand?.date}</h3>
+              <p class='text-black-100'>{brand?.history}</p>
+            </div>
           </div>
         ))}
       </div>
+
     </>
   )
 }
