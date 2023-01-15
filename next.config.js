ead5f995dc359d3fced018e87124080f8196fef8
stefang <marelestef@gmail.com>
2023-01-15 19:37:24 +0100

    redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async redirects () {
+    return [
+      {
+        source: '/',
+        destination: '/showcase',
+        permanent: true
+      }
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com']
